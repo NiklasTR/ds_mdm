d403f8f0936776afda93b3da52a840d2bb082338
NiklasTR <niklastimon@gmail.com>
2019-02-24 18:15:30 -0500

    Update parse_args.R

--- NTR4_assignment1/R/parse_args.R
+++ NTR4_assignment1/R/parse_args.R
@@ -1,6 +1,6 @@
 parse_args <- function(args){
   # Assigning variables
-  if(grepl(args[2], pattern = "c(")){
+  # if(grepl(args[2], pattern = "c(")){
     args = list(
       path = args[1],
       ctrl = eval(parse(text = args[2])),
@@ -8,13 +8,13 @@ parse_args <- function(args){
       chr_n = args[4]
     )
     return(args)
-  }
-  
-  args = list(
-    path = args[1],
-    ctrl = args[2],
-    disease = args[3],
-    chr_n = args[4])
-  
-  return(args)
+  # }
+  # 
+  # args = list(
+  #   path = args[1],
+  #   ctrl = args[2],
+  #   disease = args[3],
+  #   chr_n = args[4])
+  # 
+  # return(args)
 }
