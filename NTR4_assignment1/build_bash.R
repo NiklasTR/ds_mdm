78726eef5a1697ccfd7b86254258978cfbcbb573
NiklasTR <niklastimon@gmail.com>
2019-03-03 13:53:16 -0500

    clean jobs

--- NTR4_assignment1/build_bash.R
+++ NTR4_assignment1/build_bash.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(stringr)
 library(tidyverse)
 
-line <- read_lines(here("NTR4_assignment1/bash/template_pgr"))
+line <- read_lines(here("NTR4_assignment1/bash/template"))
 
 disease <- c("RA", "T2D", "T1D", "CAD", "CD", "BD")
 chr <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
@@ -16,5 +16,5 @@ line[length(line)] %>%
   lapply(., str_replace, pattern = "22", replacement = chr) %>% 
   unlist() %>%
   lapply(., function(x) c(line[1:length(line)-1], x)) %>% 
-  lapply(., function(x) write_lines(path = paste0(here("NTR4_assignment1/bash/"), "call_", x[length(line)] %>% str_split(pattern = "'") %>% unlist() %>% .[c(6, 8)] %>% paste(collapse = "_"), ".run"), x))
+  lapply(., function(x) write_lines(path = paste0(here("NTR4_assignment1/bash/"), "call", x[length(line)] %>% str_split(pattern = "'") %>% unlist() %>% .[c(6, 8)] %>% paste(collapse = "_"), ".run"), x))
 
